902d80d5cebc3c46e98c114a6ec0334c6264fa33
Akshay <akshaybabloo@gmail.com>
2021-08-15 19:33:55 +1200

    list command now lists paths

--- cmd/list.go
+++ cmd/list.go
@@ -1,11 +1,57 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/dustin/go-humanize"
+	"github.com/fatih/color"
+	"github.com/olekukonko/tablewriter"
+	"github.com/spf13/cobra"
+
+	"github.com/akshaybabloo/dnode/pkg"
+)
 
 func NewListCmd() *cobra.Command {
 	var listCmd = &cobra.Command{
 		Use: "list",
 		Short: "Lists out all 'node_modules' folders",
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			wd, err := os.Getwd()
+			if err != nil {
+				return err
+			}
+
+			fmt.Printf("\r%s", color.GreenString("Searching..."))
+			dirStats, err := pkg.ListDirStat("node_modules", wd)
+			if err != nil {
+				return err
+			}
+
+			if len(dirStats) == 0 {
+				fmt.Printf("\r")
+				color.Red("No 'node_modules' found")
+				return nil
+			}
+			fmt.Printf("\r")
+
+			var totalSize int64 = 0
+
+			table := tablewriter.NewWriter(os.Stdout)
+			for _, stat := range dirStats {
+				table.Append([]string{strings.ReplaceAll(stat.Path, wd, "."), humanize.Bytes(uint64(stat.Size))})
+				totalSize += stat.Size
+			}
+			table.SetHeader([]string{"Path", "Directory Size"})
+			table.SetFooter([]string{"Total", humanize.Bytes(uint64(totalSize))})
+			table.SetAutoMergeCellsByColumnIndex([]int{2, 3})
+			table.SetBorder(false)
+			table.Render()
+
+			return nil
+		},
 	}
 
 	return listCmd
--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module github.com/akshaybabloo/dnode
 go 1.16
 
 require (
-	github.com/MakeNowJust/heredoc v1.0.0 // indirect
-	github.com/spf13/cobra v1.2.1 // indirect
+	github.com/MakeNowJust/heredoc v1.0.0
+	github.com/briandowns/spinner v1.16.0 // indirect
+	github.com/dustin/go-humanize v1.0.0
+	github.com/fatih/color v1.12.0 // indirect
+	github.com/olekukonko/tablewriter v0.0.5
+	github.com/spf13/cobra v1.2.1
 )
