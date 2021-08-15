ca403359fe3b1946fa474a88c74c53318dec8f3a
Akshay <akshaybabloo@gmail.com>
2021-08-15 17:11:42 +1200

    concurrent tree size added

--- pkg/walk.go
+++ pkg/walk.go
@@ -2,10 +2,14 @@ package pkg
 
 import (
 	"errors"
+	"fmt"
 	"io/fs"
 	"os"
 	"path/filepath"
+	"sync"
 )
+
+var wg sync.WaitGroup
 
 type Directories struct {
 	Path string
@@ -22,14 +26,63 @@ func ListDirStat(keyword string, dirPath string) ([]Directories, error) {
 	if !pathStat.IsDir() {
 		return nil, errors.New("the path provided is not a directory")
 	}
+	dirChan := make(chan Directories, 1)
+	var directories []Directories
 
-	err = filepath.WalkDir(dirPath, func(path string, d fs.DirEntry, err error) error {
+	visit := func(path string, d fs.DirEntry, err error) error {
+		if d.IsDir() && d.Name() == keyword {
+			fmt.Println(path)
+			wg.Add(1)
 
+			go func(path string) {
+				size, _ := getTreeSize(path)
+				dirChan <- Directories{
+					Path: path,
+					Size: size,
+				}
+			}(path)
+			return filepath.SkipDir
+		}
 		return err
-	})
+	}
+
+	err = filepath.WalkDir(dirPath, visit)
 	if err != nil {
 		return nil, err
 	}
 
-	return []Directories{}, nil
+	go func() {
+		for i := range dirChan {
+			directories = append(directories, i)
+			wg.Done()
+		}
+	}()
+
+	wg.Wait()
+
+	return directories, nil
+}
+
+func getTreeSize(path string) (int64, error) {
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		return 0, err
+	}
+	var total int64
+	for _, entry := range entries {
+		if entry.IsDir() {
+			size, err := getTreeSize(filepath.Join(path, entry.Name()))
+			if err != nil {
+				return 0, err
+			}
+			total += size
+		} else {
+			info, err := entry.Info()
+			if err != nil {
+				return 0, err
+			}
+			total += info.Size()
+		}
+	}
+	return total, nil
 }
