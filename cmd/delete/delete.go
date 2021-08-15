35c3aad5bc5e0d315af30237bb5cf87b69e242f8
Akshay <akshaybabloo@gmail.com>
2021-08-15 20:21:46 +1200

    delete command added

--- cmd/delete/delete.go
+++ cmd/delete/delete.go
@@ -1,12 +1,57 @@
 package delete
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/dustin/go-humanize"
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+
+	"github.com/akshaybabloo/dnode/pkg"
+)
+
+var wd string
 
 func NewDeleteCmd() *cobra.Command {
 	var deleteCmd = &cobra.Command{
 		Use:   "delete",
 		Short: "Delete all 'node_modules' folders",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			var err error
+			if wd == "" {
+				wd, err = os.Getwd()
+				if err != nil {
+					return err
+				}
+			}
+
+			fmt.Printf("\r%s", color.GreenString("Searching..."))
+			dirStats, err := pkg.ListDirStat("node_modules", wd)
+			if err != nil {
+				return err
+			}
+
+			if len(dirStats) == 0 {
+				fmt.Printf("\r")
+				color.Red("No 'node_modules' found")
+				return nil
+			}
+			fmt.Printf("\r")
+
+			var totalSize int64 = 0
+
+			for _, stat := range dirStats {
+				err := os.RemoveAll(stat.Path)
+				if err != nil {
+					return err
+				}
+				fmt.Printf("Deleted %s\n", strings.ReplaceAll(stat.Path, wd, "."))
+				totalSize += stat.Size
+			}
+
+			color.Green("%s freed", humanize.Bytes(uint64(totalSize)))
 
 			return nil
 		},
